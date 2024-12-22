3b6ccf19b2baf9dd67a212eec8629f6c25408908
Supriyo Das <das.supriyo07@gmail.com>
2024-12-22 21:15:06 +0530

    Conditional Statement

---
+++ 08.conditional_statement.php
@@ -0,0 +1,15 @@
+<?php
+$result = 85;
+
+if($result >= 80){
+    echo "Grade : A";
+}elseif ($result >= 60) {
+    echo "Grade : B";
+}elseif($result >= 40){
+    echo "Grade : C";
+}elseif(reset >= 34){
+    echo "Grade : D";
+}else{
+    echo "Fail";
+}
+?>
