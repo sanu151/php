4794d61547bc0fb36200312bc517ca5911645bfa
Supriyo Das <das.supriyo07@gmail.com>
2024-12-24 22:18:34 +0530

    While Loop

---
+++ 15.whileLoop.php
@@ -0,0 +1,12 @@
+<?php
+    // While Loop
+
+    $i = 1;
+    $num = 8;
+
+    while($i <= 10){
+        echo "$num x $i = " . $num * $i . "<br>";
+        $i++;
+    };
+    echo "Ends of the Loop"
+?>
