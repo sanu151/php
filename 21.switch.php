d9b4279d80ea8ccc2bc8117f1c11610a94a6339e
Supriyo Das <das.supriyo07@gmail.com>
2024-12-25 20:31:03 +0530

    switch statement

---
+++ 21.switch.php
@@ -0,0 +1,31 @@
+<?php
+    $day = "Wednesday";
+
+    switch($day){
+        case "Monday":
+            echo "Today is Monday, the first day of the week";
+            break;
+        case "Tuesday":
+            echo "Today is Tuesday, the second day of the week";
+            break;
+        case "Wednesday":
+            echo "Today is Wednesday, the third day of the week";
+            break;
+        case "Thursday":
+            echo "Today is Thursday, the fourth day of the week";
+            break;
+        case "Friday":
+            echo "Today is Friday, the fifth day of the week";
+            break;
+        case "Saturday":
+            echo "Today is Saturday, the sixth day of the week";
+            break;
+        case "Sunday":
+            echo "Today is Sunday, the seventh day of the week";
+            break;
+        default:
+            echo "Invalid Day, please check the day";
+    }
+
+
+?>
