0317b269e27be6b48e240cc744c82dd3e52908ec
Supriyo Das <das.supriyo07@gmail.com>
2024-12-26 22:18:56 +0530

    Case changin methods (uppercase, lowercase etc.)

---
+++ 24.caseChangingMethods.php
@@ -0,0 +1,15 @@
+<?php
+$str = "Hello World!";
+$str1 = "supriyo das";
+
+// to lower case
+echo strtolower($str)."<br>";
+// to upper case
+echo strtoupper($str)."<br>";
+// to first letter lower case
+echo lcfirst($str)."<br>";
+// to first letter upper case
+echo ucfirst($str1)."<br>";
+// to first letter of each word upper case
+echo ucwords($str1)."<br>";
+?>
